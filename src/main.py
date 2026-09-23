87be1e11ccbe1ff2289911768120f0173a46b49e
Drenae <adrien.guilpain@gmail.com>
2026-09-23 07:57:16 +0200

    feat: initialize modular application architecture

--- src/main.py
+++ src/main.py
@@ -1,25 +1,10 @@
 import flet as ft
 
+from app.app import SubForgeApp
 
-def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
 
-    def increment_click(e: ft.Event[ft.FloatingActionButton]):
-        counter.data += 1
-        counter.value = str(counter.data)
-
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, key="increment", on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            expand=True,
-            content=ft.Container(
-                content=counter,
-                alignment=ft.Alignment.CENTER,
-            ),
-        )
-    )
+def main(page: ft.Page) -> None:
+    SubForgeApp(page).run()
 
 
 if __name__ == "__main__":
