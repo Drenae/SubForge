8d74a6f548d051115fb43414ba446ade01ac9837
Drenae <adrien.guilpain@gmail.com>
2026-09-26 18:29:52 +0200

    Add AppNavigationButton class for navigation

---
+++ src/app/layout/app_navigation_button.py
@@ -0,0 +1,25 @@
+from collections.abc import Callable
+
+import flet as ft
+
+
+class AppNavigationButton(ft.Button):
+    def __init__(self, text=None, icon=None, route=str, on_navigate= Callable[[str], None], **kwargs):
+        super().__init__(**kwargs)
+        self.content = ft.Text(text)
+        self.color = ft.Colors.BLACK
+        self.icon = ft.Icon(icon, size=24)
+        self.icon_color = ft.Colors.BLACK
+        self.style = ft.ButtonStyle(
+            padding=ft.Padding.symmetric(horizontal=12, vertical=14),
+            shape=ft.RoundedRectangleBorder(radius=6),
+            side={
+                ft.ControlState.DEFAULT: ft.BorderSide(2, ft.Colors.with_opacity(0.6, ft.Colors.AMBER)),
+                ft.ControlState.HOVERED: ft.BorderSide(2, ft.Colors.AMBER)
+            },
+            bgcolor={
+                ft.ControlState.DEFAULT: ft.Colors.with_opacity(0.6, ft.Colors.AMBER),
+                ft.ControlState.HOVERED: ft.Colors.AMBER,
+            },
+        )
+        self.on_click = lambda _: on_navigate(route)
